352c32b352c8ff8c27e79684c3b26032257f419b
shadmar <shadmar@gmail.com>
2013-06-26 01:38:41 +0200

    more tests crashes still...

--- data/modules/Fulcrums.lua
+++ data/modules/Fulcrums.lua
@@ -4,6 +4,22 @@
 local loaded
 local fulcrum
 local playerarrived=false
+local turrets={}
+local turret_target=nil
+
+local spawnTurrets = function (ship)
+ 	Timer:CallAt(Game.time+1, function ()
+		if ship==nil or not ship:exists() then return end
+		for x=1,5 do
+			turrets[x]=Space.SpawnShipNear('military_gun_emplacement', fulcrum, 1, 1)
+			turrets[x]:AddEquip('PULSECANNON_20MW')
+			turrets[x]:AddEquip('PULSECANNON_20MW')
+			turrets[x]:AddEquip('LASER_COOLING_BOOSTER')
+			turrets[x]:SetLabel('[Military Sentry]')
+			turrets[x]:AIHoldPos()
+		end
+	end)
+end
 
 local spawnShips = function ()
 	local population = Game.system.population
@@ -25,6 +41,8 @@ local spawnShips = function ()
 	fulcrum:AddEquip("ECM_ADVANCED")
 
 	playerarrived=true
+
+	spawnTurrets(fulcrum)
 	
 	return 0
 end
@@ -45,14 +63,19 @@ local onEnterSystem = function (player)
 			y=y+200
 			player:SetPos(fulcrum,x,y,z)
 			fulcrum:UseECM()
-			local p=Space.SpawnShipNear('military_gun_emplacement', Game.player, 1, 1)
-			p:AddEquip('PULSECANNON_DUAL_1MW')
-			p:AddEquip('LASER_COOLING_BOOSTER')
-			p:SetLabel('[Military Sentry]')
-			p:SetFuelPercent(0)
-			p:AIHoldPos()
-			p:AIKill(Game.player)
 		end
+	end
+end
+
+local onShipDestroyed = function (ship, attacker)
+	if turrets~=nil and ship==turret_target then 
+		for k,v in pairs(turrets) do
+			if v==attacker then
+				v:CancelAI()
+				v:AIHoldPos()
+			end
+		end
+		--turret_target = nil
 	end
 end
 
@@ -60,13 +83,23 @@ local onAICompleted = function (ship, ai_error)
 	if not ship:IsPlayer() and playerarrived==true then return end
 	playerarrived=false
 end
-Event.Register("onAICompleted", onAICompleted)
+
 
 local onGameStart = function ()
 	if loaded == nil then
 		spawnShips()
 	end
 	loaded = nil
+end
+
+local onShipHit = function (ship, attacker)
+	if ship==nil or not ship:exists() or ship==attacker then return end
+	if turrets==nil then return end
+	for k,v in pairs(turrets) do
+		if v~=nil and v:exists() and v:isa('Ship') and (ship==fulcrum or v==ship or ship==Game.player) then
+			v:AIKill(attacker)
+		end
+	end
 end
 
 local serialize = function ()
@@ -77,14 +110,18 @@ local unserialize = function (data)
 	loaded = true
 	playerarrived=data[1]
 	fulcrum=data[2]
+	spawnTurrets(fulcrum)
 end
 
 local onGameEnd = function ()
 	-- drop the references for our data so Lua can free them
 	-- and so we can start fresh if the player starts another game
-	loaded,fulcrum,playerarrived=nil,nil,nil
+	loaded,fulcrum,playerarrived,turrets=nil,nil,nil,nil
 end
 
+Event.Register("onShipDestroyed", onShipDestroyed)
+Event.Register("onAICompleted", onAICompleted)
+Event.Register("onShipHit", onShipHit)
 Event.Register("onGameEnd", onGameEnd)
 Event.Register("onEnterSystem", onEnterSystem)
 Event.Register("onGameStart", onGameStart)
