ae31245ceef8a4c1cfc24cd942b259c9853d3646
shadmar <shadmar@gmail.com>
2013-06-26 22:11:15 +0200

    alot was done here.. maybe not stabile yet..

--- data/modules/Fulcrums.lua
+++ data/modules/Fulcrums.lua
@@ -4,6 +4,26 @@
 local loaded
 local fulcrum
 local playerarrived=false
+local turrets={}
+local turret_target=nil
+
+local spawnTurrets = function (ship)
+ 	Timer:CallAt(Game.time+1, function ()
+		if ship==nil or not ship:exists() then return end
+		for x=1,5 do
+			local t	=Space.SpawnShipNear('military_gun_emplacement', fulcrum, 1, 1)
+			turrets[t] = {
+				ship	= t,
+				status	='idle',
+			}
+			turrets[t].ship:AddEquip('PULSECANNON_20MW')
+			turrets[t].ship:AddEquip('PULSECANNON_20MW')
+			turrets[t].ship:AddEquip('LASER_COOLING_BOOSTER')
+			turrets[t].ship:SetLabel('[Military Sentry]')
+			turrets[t].ship:AIHoldPos()
+		end
+	end)
+end
 
 local spawnShips = function ()
 	local population = Game.system.population
@@ -25,6 +45,8 @@ local spawnShips = function ()
 	fulcrum:AddEquip("ECM_ADVANCED")
 
 	playerarrived=true
+
+	spawnTurrets(fulcrum)
 	
 	return 0
 end
@@ -49,11 +71,39 @@ local onEnterSystem = function (player)
 	end
 end
 
+local onShipDestroyed = function (ship, attacker)
+
+end
+
 local onAICompleted = function (ship, ai_error)
+
+	if ship==nil then return end
+	if not ship:exists() then return end
+
+	if turrets~=nil then 
+		for k,v in pairs(turrets) do
+			if ship==v.ship and fulcrum:exists() then
+				if v.status=='attacking' then
+					v.ship:CancelAI()
+					v.status='cancel'
+					print ('got aicomplete...')
+					Timer:CallAt(Game.time+1, function ()
+						if v.ship~=nil and v.ship:exists() and v.status=='cancel' then
+							v.ship:AIHoldPos()
+							v.status='idle'
+							print ('aicomplete:idle')
+						end
+					end)
+				end
+			end
+		end
+		--turret_target = nil
+	end
+
 	if not ship:IsPlayer() and playerarrived==true then return end
 	playerarrived=false
 end
-Event.Register("onAICompleted", onAICompleted)
+
 
 local onGameStart = function ()
 	if loaded == nil then
@@ -62,16 +112,80 @@ local onGameStart = function ()
 	loaded = nil
 end
 
+local onShipHit = function (ship, attacker)
+	if ship==nil or not ship:exists() or ship==attacker then return end
+	if turrets==nil then return end
+	for k,v in pairs(turrets) do
+		--attacker exists and turret in range:
+		if attacker~=nil and v~=nil and attacker:exists() and v.ship:exists() and v.ship:isa('Ship') and (ship==fulcrum or v.ship==ship or ship==Game.player) and attacker:DistanceTo(ship)<4000 then
+			--on cancel attack:
+			if v.status=='cancel' then
+				v.ship:AIKill(attacker)
+				turret_target=attacker
+				v.status='attacking'
+				print ('attacking ..'..attacker.label)
+			end
+			-- on idle, cancel
+			if v.status=='idle' then
+				v.ship:CancelAI()
+				v.status='cancel'
+				print ('got cancel ..')
+			end
+		else
+
+			-- attacker exists out of range
+			if attacker~=nil and v.ship~=nil and v.ship:exists() and attacker:exists() and turret_target~=nil and turret_target~=attacker then --using turret target to determine out of range
+				v.status='norange'
+				print ('got out of range..')
+				Timer:CallAt(Game.time+1, function ()
+					if v.ship~=nil and v.ship:exists() and v.status=='norange' then
+							v.ship:AIHoldPos()
+							v.status='idle'
+							print ('norange:idle:holdpos')
+					end
+				end)
+			end
+
+
+			-- attacker is gone..
+			if attacker==nil and v.ship~=nil and v.ship:exists() then 
+				v.status='noattacker' 
+				print ('got no attacker...')
+				Timer:CallAt(Game.time+1, function ()
+					if v.ship~=nil and v.ship:exists() and v.status=='noattacker' then
+							v.ship:AIHoldPos()
+							v.status='idle'
+							print ('noattacker:idle:holdpos')
+					end
+				end)
+			end
+		end
+
+	
+	end
+end
+
 local serialize = function ()
-	return playerarrived,fulcrum
+	return {playerarrived,fulcrum}
 end
 
 local unserialize = function (data)
 	loaded = true
-	playerarrived,fulcrum = data
+	playerarrived=data[1]
+	fulcrum=data[2]
+	spawnTurrets(fulcrum)
 end
 
+local onGameEnd = function ()
+	-- drop the references for our data so Lua can free them
+	-- and so we can start fresh if the player starts another game
+	loaded,fulcrum,playerarrived,turrets=nil,nil,nil,nil
+end
+
+Event.Register("onShipDestroyed", onShipDestroyed)
+Event.Register("onAICompleted", onAICompleted)
+Event.Register("onShipHit", onShipHit)
+Event.Register("onGameEnd", onGameEnd)
 Event.Register("onEnterSystem", onEnterSystem)
 Event.Register("onGameStart", onGameStart)
-
 Serializer:Register("Fulcrum", serialize, unserialize)
