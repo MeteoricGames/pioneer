44e6213717025e69e4849596240f00f6bbc0247a
shadmar <shadmar@gmail.com>
2013-06-26 21:47:08 +0200

    sentry adjusted

--- data/ships/(5)Emplacements/military_gun_emplacement.lua
+++ data/ships/(5)Emplacements/military_gun_emplacement.lua
@@ -11,7 +11,7 @@ define_static_ship {
 	down_thrust = 800e5,
 	left_thrust = 800e5,
 	right_thrust = 800e5,
-	angular_thrust = 250000e5,
+	angular_thrust = 18000e5,
 	gun_mounts =
 	{
 		{ v(0,0,-150), v(0,0,-1), 5, 'HORIZONTAL' },
