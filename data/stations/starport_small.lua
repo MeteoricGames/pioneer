07df24493e7838ae2905e4bfb56428779633c28e
Lionheart <lionheart1588@gmail.com>
2013-07-10 23:52:46 +0100

    small starport model and defs

---
+++ data/stations/starport_small.lua
@@ -0,0 +1,17 @@
+define_surface_station {
+	model = 'starport_small',
+    num_docking_ports = 4,
+	-- define groups of bays, in this case 1 group with 1 bay.
+	-- params are = {minSize, maxSize, {list,of,bay,numbers}}
+	bay_groups = {
+		{20, 50, {1}},
+		{20, 50, {2}},
+		{20, 50, {3}},
+		{20, 50, {4}},
+	},
+    parking_distance = 5000.0,
+    parking_gap_size = 2000.0,
+    ship_launch_stage = 0,
+    dock_anim_stage_duration = { 300, 4.0},
+    undock_anim_stage_duration = { 0 },
+}
