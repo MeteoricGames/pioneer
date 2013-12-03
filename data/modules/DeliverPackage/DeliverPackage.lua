1ccecc96e547255c3078881d36bfff5f802292d1
shadmar <shadmar@gmail.com>
2013-12-03 20:48:45 +0100

    Reduce mission times to a fraction

--- data/modules/DeliverPackage/DeliverPackage.lua
+++ data/modules/DeliverPackage/DeliverPackage.lua
@@ -27,7 +27,7 @@ local ui = Engine.ui
 local max_delivery_dist = 30
 -- typical time for travel to a system max_delivery_dist away
 --	Irigi: ~ 4 days for in-system travel, the rest is FTL travel time
-local typical_travel_time = (1.6 * max_delivery_dist + 4) * 24 * 60 * 60
+local typical_travel_time = (1.6 * max_delivery_dist + 4) * 24 * 2 * 2
 -- typical reward for delivery to a system max_delivery_dist away
 local typical_reward = 25 * max_delivery_dist
 
@@ -190,7 +190,7 @@ local makeAdvert = function (station)
 		dist = station:DistanceTo(locdist)
 		if dist < 1000 then return end
 		reward = 25 + (math.sqrt(dist) / 15000) * (1+urgency)
-		due = Game.time + ((4*24*60*60) * (Engine.rand:Number(1.5,3.5) - urgency))
+		due = Game.time + ((4*24*2*2) * (Engine.rand:Number(1.5,3.5) - urgency))
 	else
 		if nearbysystems == nil then
 			nearbysystems = Game.system:GetNearbySystems(max_delivery_dist, function (s) return #s:GetStationPaths() > 0 end)
@@ -240,14 +240,14 @@ end
 local onUpdateBB = function (station)
 	for ref,ad in pairs(ads) do
 		if flavours[ad.flavour].localdelivery == 0
-			and ad.due < Game.time + 5*60*60*24 then -- five day timeout for inter-system
+			and ad.due < Game.time + 5*2*2*24 then -- five day timeout for inter-system
 			ad.station:RemoveAdvert(ref)
 		elseif flavours[ad.flavour].localdelivery == 1
-			and ad.due < Game.time + 2*60*60*24 then -- two day timeout for locals
+			and ad.due < Game.time + 2*2*2*24 then -- two day timeout for locals
 			ad.station:RemoveAdvert(ref)
 		end
 	end
-	if Engine.rand:Integer(12*60*60) < 60*60 then -- roughly once every twelve hours
+	if Engine.rand:Integer(12*2*2) < 2*2 then -- roughly once every twelve hours
 		makeAdvert(station)
 	end
 end
