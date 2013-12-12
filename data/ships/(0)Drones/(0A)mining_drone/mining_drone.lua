31513a68259bcd7988efcd4b607d4c0df3414378
shadmar <shadmar@gmail.com>
2013-12-12 21:15:54 +0100

    re add gunmounts

--- data/ships/(0)Drones/(0A)mining_drone/mining_drone.lua
+++ data/ships/(0)Drones/(0A)mining_drone/mining_drone.lua
@@ -14,10 +14,14 @@ define_drone {
 	max_cargo = 30,
 	max_laser = 1,
 	max_missile = 0,
+ 	gun_mounts =
+        {
+                { v(0,0,0), v(0,0,-1), 5, 'HORIZONTAL' },
+        },
 	max_cargoscoop = 1,
 	max_fuelscoop = 0,
 	min_crew = 1,
-	max_crew = 2,
+	max_crew = 1,
 	capacity = 30,
 	hull_mass = 30,
 	fuel_tank_mass = 25,
