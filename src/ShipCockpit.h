d4b4833d0db753b5a7fe9a92c4a64f63f900a1da
Salwan <salwan@peakgames.net>
2013-12-19 17:32:22 +0200

    Added cockpit lag on acceleration

--- src/ShipCockpit.h
+++ src/ShipCockpit.h
@@ -10,7 +10,10 @@
 struct ShipType;
 
 static const float COCKPIT_LAG_MAX_ANGLE = 7.5f;
-static const float COCKPIT_INTERPOLATION_MULTIPLIER = 5.0f;
+static const float COCKPIT_ROTATION_INTERP_MULTIPLIER = 5.0f;
+static const float COCKPIT_ACCEL_INTERP_MULTIPLIER = 0.5f;
+static const float COCKPIT_MAX_GFORCE = 10000.0f;
+static const float COCKPIT_ACCEL_OFFSET = 0.125f;
 
 enum CockpitLagEasing
 {
@@ -46,8 +49,13 @@ private:
 	vector3d vShipYaw;		// current ship yaw vector
 	vector3d vdDir;			// cockpit direction
 	vector3d vdYaw;			// cockpit yaw vector
-	float fInterp;			// for motion interpolation
+	float fRInterp;			// for rotation interpolation
+	float fTInterp;			// for translation interpolation
+	float fGForce;			// current ship gforce
+	float fOffset;			// current ship offset due to acceleration effect
+	float fShipVel;			// current ship velocity
 	CockpitLagEasing eEasing; // Easing function for lag recover
+	vector3d vTranslate;    // cockpit translation
 	matrix4x4d matTransform;// cockpit transformation
 };
 
