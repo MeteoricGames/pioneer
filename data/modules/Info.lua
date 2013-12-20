2ad3ac0dcf2b694749b5b21116cdff9b50645122
Shadmar <shadmar@gmail.com>
2013-12-20 21:39:18 +0100

    Basic system info

--- data/modules/Info.lua
+++ data/modules/Info.lua
@@ -4,9 +4,14 @@ local Event = import("Event")
 local Game = import("Game")
 local Space = import("Space")
 
+function round(num, idp)
+  local mult = 10^(idp or 0)
+  return math.floor(num * mult + 0.5) / mult
+end
+
 local onEnterSystem = function (ship)
 	if ship~=Game.player then return end
-	Comms.ImportantInfo("System : xyz \nPopulation : xyz \nOther : styff")
+	Comms.ImportantInfo("System : "..Game.system.path:GetStarSystem().name.."\nPopulation :"..round(Game.system.population,2).." billion\nSecurity Rating : "..round(1-Game.system.lawlessness,2).."")
 	Comms.Info(" ")
 end
 Event.Register("onEnterSystem", onEnterSystem)
