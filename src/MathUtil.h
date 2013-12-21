5814780a643af81f7a90d5b32a7f7945116f2f01
Salwan <salwan@peakgames.net>
2013-12-21 17:53:12 +0200

    Added two missing ease-in functions

--- src/MathUtil.h
+++ src/MathUtil.h
@@ -32,8 +32,18 @@ inline T QuadInterpOut(T a, T b, T delta) {
 }
 
 template <typename T>
+inline T ExpInterpIn(T a, T b, T delta) {
+	return (b - a) * pow(2, 10 * (delta - 1)) + a;
+}
+
+template <typename T>
 inline T ExpInterpOut(T a, T b, T delta) {
 	return (b - a) * (-pow(2, -10 * delta) + 1) + a;
+}
+
+template <typename T>
+inline T CubicInterpIn(T a, T b, T delta) {
+	return ((b - a) * delta * delta * delta) + a;
 }
 
 template <typename T>
