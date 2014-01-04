2a10ee1bd236c387ae529df51a6614089e43ed3e
Shadmar <shadmar@gmail.com>
2014-01-04 14:07:56 +0100

    Revert "Temporary solution for z-order cockpit issue"
    This reverts commit caddba46282af926f076694acc29979cae36e5a1.

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -196,12 +196,11 @@ void Camera::Draw(Renderer *renderer, const Body *excludeBody, ModelBody* cockpi
 
 	// Render cockpit
 	if(cockpit) {
-		//renderer->SetDepthTest(false);
-		renderer->ClearDepthBuffer();
+		renderer->SetDepthTest(false);
 		cockpit->SetFrame(m_camFrame);
 		cockpit->Render(renderer, this, vector3d(0, 0, 0), matrix4x4d::Identity());
 		cockpit->SetFrame(nullptr);
-		//renderer->SetDepthTest(true);
+		renderer->SetDepthTest(true);
 	}
 
 	m_frame->RemoveChild(m_camFrame);
